acc89deb3d982df45cdacd3b1232d635ac26f06c
Redfluffydragon <kderu1420@gmail.com>
2022-02-06 14:53:45 -0700

    Group events into years and move inputs into a footer

--- src/Timeline.svelte
+++ src/Timeline.svelte
@@ -1,13 +1,15 @@
 <script>
+	import FileInput from './FileInput.svelte';
   import TimelineEvent from './TimelineEvent.svelte';
   import YearColumn from './YearColumn.svelte';
   import { events, data } from "./stores";
   import domtoimage from 'dom-to-image';
-  
+
   $: $events = $events.sort((a, b) => a['Graph years'] - b['Graph years']);
   let sortedEvents = {
     unknown: [],
   };
+
   $: $events.forEach(item => {
     if (isNaN(item['Graph years'])) {
       sortedEvents.unknown.push(item);
@@ -20,63 +22,56 @@
     sortedEvents[item['Graph years']].push(item);
   });
 
-  $: yearKeys = Object.keys(sortedEvents).sort();
-
-  $: console.log(yearKeys);
-
-  $: $data.start = $events.length ? parseInt($events[0]['Graph years']) : 0;
-  $: $data.end = $events.length ? parseInt($events[$events.findIndex(item => isNaN(item['Graph years'])) - 1]['Graph years']) : 0;
-  $: $data.range = $data.end - $data.start;
+  $: $data.start = $events.length ? parseInt($events[0]['Graph years']) : 1520;
+  $: $data.end = $events.length ? parseInt($events[$events.findIndex(item => isNaN(item['Graph years'])) - 1]['Graph years']) : 1530;
+  $: $data.range = $data.end - $data.start + 1; // Plus one to include the end year
 
   $: allYears = Array.from(Array($data.range).keys()).map(year => year + $data.start);
-  $: console.log(allYears);
 
   let timelineNode;
 </script>
 
-<div class="timelineWrapper" bind:this={timelineNode}>
-  <div class="timeline">
-    {#if $events.length}
-      {#each allYears as year}
-        <YearColumn>
-          {#if sortedEvents[year]}
-            {#each sortedEvents[year] as event}
-              <TimelineEvent {event} />
-            {/each}
-          {/if}
-        </YearColumn>
-      {/each}
-      <YearColumn>
-        {#each sortedEvents.unknown as event}
-          <TimelineEvent {event} />
+<main>
+  <div class="timelineWrapper" bind:this={timelineNode}>
+    <div class="timeline">
+      {#if $events.length}
+        {#each allYears as year}
+          <YearColumn year={sortedEvents[year] ? year : null}>
+            {#if sortedEvents[year]}
+              {#each sortedEvents[year] as event}
+                <TimelineEvent {event} />
+              {/each}
+            {/if}
+          </YearColumn>
         {/each}
-      </YearColumn>
-    {/if}
+        <YearColumn year="Unknown">
+          {#each sortedEvents.unknown as event}
+            <TimelineEvent {event} />
+          {/each}
+        </YearColumn>
+      {/if}
+    </div>
   </div>
-</div>
+</main>
 
-<button on:click={() => {
-  /* domtoimage.toPng(timelineNode)
-  .then(dataUrl => {
-        var img = document.createElement('img');
-        img.src = dataUrl;
-        document.body.appendChild(img);
-    })
-    .catch(error => {
-        console.error('oops, something went wrong!', error);
-    }); */
-  domtoimage.toBlob(timelineNode)
-  .then(blob => {
-    console.log(blob);
-    const downloadLink = document.createElement('a');
+<div class="spacer"></div>
 
-    const url = window.URL.createObjectURL(blob);
-    downloadLink.href = url;
-    downloadLink.download = 'timeline.png';
-    downloadLink.click();
-    window.URL.revokeObjectURL(url);
-  });
-}}>Export to png</button>
+<footer>
+  <FileInput />
+  
+  <button on:click={() => {
+    domtoimage.toBlob(timelineNode)
+    .then(blob => {
+      const downloadLink = document.createElement('a');
+  
+      const url = window.URL.createObjectURL(blob);
+      downloadLink.href = url;
+      downloadLink.download = 'timeline.png';
+      downloadLink.click();
+      window.URL.revokeObjectURL(url);
+    });
+  }}>Export to png</button>
+</footer>
 
 <style>
   .timelineWrapper {
@@ -85,7 +80,22 @@
 
   .timeline {
     display: flex;
+  }
+
+  .spacer {
+    height: 6em;
+  }
+
+  footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    padding: 2ch;
+    background: white;
+    box-shadow: 0 0 15px black;
+    display: flex;
     gap: 2ch;
-    position: relative;
+    place-content: center;
   }
 </style>
