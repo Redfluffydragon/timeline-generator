742fd325ab7ae97e8f3e8e5b9d37729163fae159
Redfluffydragon <kderu1420@gmail.com>
2022-02-06 14:53:06 -0700

    Add a YearColumn component to group events into years

--- src/YearColumn.svelte
+++ src/YearColumn.svelte
@@ -1,7 +1,11 @@
 <script>
+  export let year = null;
 </script>
 
 <div class="column">
+  {#if year}
+    <h2>{year}</h2>
+  {/if}
   <slot />
 </div>
 
@@ -11,5 +15,25 @@
     flex-direction: column;
     gap: 2ch;
     width: 25ch;
+    border-top: 2ch solid #000;
+    padding: 2em 1ch 1ch;
+  }
+
+  h2 {
+    position: relative;
+    margin-bottom: 0;
+    text-align: center;
+  }
+
+  h2::before {
+    position: absolute;
+    top: -2.5em;
+    /* left: 1.5ch; */
+    left: 50%;
+    transform: translateX(-50%);
+    content: '';
+    background: black;
+    width: 1ch;
+    height: 2em;
   }
 </style>
