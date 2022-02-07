6839720bb68a1db35e070cebca5b164aad9ac51b
Redfluffydragon <kderu1420@gmail.com>
2022-02-06 18:13:20 -0700

    Add sheetData store for selecting columns and sheets

--- src/stores.js
+++ src/stores.js
@@ -2,7 +2,14 @@ import { writable } from "svelte/store";
 
 export const events = writable([]);
 
-export const data = writable({
+export const dateData = writable({
   range: 0,
   start: 0,
-})
+});
+
+export const sheetData = writable({
+  sheet: '',
+  dateColumn: '',
+  titleColumn: 'illegal/sheetname',
+  descriptionColumn: 'illegal/sheetname',
+});
